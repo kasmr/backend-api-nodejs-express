239afb12ec94c37b0d94334ef12135bf83155131
kasmr <alikkras@gmail.com>
2019-12-31 19:41:37 +0300

    halfway through

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
+
+// Connect Database
+connectDB();
+
+// Init Middleware
+app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.json({ msg: 'welcome to contact keeper API' }));
 
 //Define Routes
 app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth.js'));
-app.use('/api/contacts', require('./routes/contacts.js'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
 const PORT = process.env.PORT || 5000;
 
